c8140ec3f895402dcbfd9523aaf4a8eac4dab76b
Michael Grenier <grenim01@uqat.ca>
2020-02-20 13:35:27 -0500

    Vector2 addition!

--- math/src/Vector2.rs
+++ math/src/Vector2.rs
@@ -1,4 +1,5 @@
 use std::ops::{ Add };
+use std::cmp::PartialEq;
 use std::f32;
 
 #[derive(Debug)]
@@ -12,14 +13,17 @@ impl Vector2 {
 		Vector2 { x, y }
 	}
 
+	#[inline]
 	fn lengthSquared(self) -> f32 {
 		self.x * self.x + self.y * self.y
 	}
 
+	#[inline]
 	fn length(self) -> f32 {
 		self.lengthSquared().sqrt()
 	}
 
+	#[inline]
 	fn lengthManhattan(self) -> f32 {
 		self.x.abs() + self.y.abs()
 	}
@@ -34,26 +38,44 @@ impl Vector2 {
 	}
 }
 
-impl Add for Vector2 {
-	type Output = Self;
+impl PartialEq for Vector2 {
+	fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
 
-	fn add(self, other: Self) -> Vector2 {
+impl Add<Vector2> for Vector2 {
+	type Output = Vector2;
+
+	fn add(self, other: Vector2) -> Vector2 {
 		Vector2::new(self.x + other.x, self.y + other.y)
 	}
 }
 
 impl Add<f32> for Vector2 {
-	type Output = Self;
+	type Output = Vector2;
 
 	fn add(self, other: f32) -> Vector2 {
 		Vector2::new(self.x + other, self.y + other)
 	}
 }
 
+impl Add<Vector2> for f32 {
+	type Output = Vector2;
+
+	fn add(self, other: Vector2) -> Vector2 {
+		Vector2::new(other.x + self, other.y + self)
+	}
+}
+
 #[cfg(test)]
 mod tests {
+	use super::Vector2;
+
     #[test]
     fn it_adds() {
-        assert_eq!(Vector2::new(0, 0) + Vector2::new(1, 1), Vector2::new(1, 1));
+		assert_eq!(Vector2::new(0.0, 0.0) + Vector2::new(1.0, 1.0), Vector2::new(1.0, 1.0));
+		assert_eq!(Vector2::new(0.0, 0.0) + 2.0, Vector2::new(2.0, 2.0));
+		assert_eq!(2.0 + Vector2::new(0.0, 0.0), Vector2::new(2.0, 2.0));
     }
 }
