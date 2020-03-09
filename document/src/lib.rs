d13af540741c1fde4531e0c7a4cdc9d31d5a8632
Michael Grenier <grenim01@uqat.ca>
2020-03-09 13:25:32 -0400

    Moar learnig

--- document/src/lib.rs
+++ document/src/lib.rs
@@ -1,63 +1,81 @@
-pub struct Document {
-	pub root: Group,
+use std::sync::Arc;
+use uuid::Uuid;
+use im::Vector as List;
+use math::{ Rect, Vec2 };
+
+pub struct Patch {
+	target: Uuid,
+	payload: PatchPayload,
 }
 
-pub trait Node {}
+pub enum PatchPayload {
+	Rename(String),
+	Resize(u32, u32)
+}
+
+pub trait Patchable {
+	fn apply_patch(&self, patch: &Patch) -> Option<Self>
+	where
+		Self: Sized;
+}
+
+pub trait Document: Patchable {
+	fn get_bounds(&self) -> Rect;
+}
 
 pub struct Group {
+	pub id: Uuid,
 	pub name: String,
-	pub children: Vec<Box<dyn Node>>,
+	pub children: Arc<List<Arc<Box<dyn Document>>>>,
 }
 
-impl Node for Group {}
+impl Patchable for Group {
+	fn apply_patch(&self, patch: &Patch) -> Option<Self> {
+		if patch.target == self.id {
+			match &patch.payload {
+				PatchPayload::Rename(new_name) => Some(Group { id: self.id, name: new_name.clone(), children: self.children.clone() }),
+				_ => None
+			}
+		} else {
+			None
+		}
+	}
+}
+
+impl Document for Group {
+	fn get_bounds(&self) -> Rect {
+		Rect::new(Vec2::new(0., 0.), Vec2::new(0., 0.))
+	}
+}
 
 pub struct Label {
+	pub id: Uuid,
 	pub name: String,
 }
 
-impl Node for Label {}
-
-pub struct Sprite {
-	pub name: String,
-	pub layers: Vec<Box<dyn SpriteLayer>>,
-	pub width: i32,
-	pub height: i32,
+impl Patchable for Label {
+	fn apply_patch(&self, patch: &Patch) -> Option<Self> {
+		if patch.target == self.id {
+			match &patch.payload {
+				PatchPayload::Rename(new_name) => Some(Label { id: self.id, name: new_name.clone() }),
+				_ => None
+			}
+		} else {
+			None
+		}
+	}
 }
 
-impl Node for Sprite {}
+#[cfg(test)]
+mod tests {
+	use super::*;
 
-pub trait SpriteLayer {}
-
-pub struct SpriteLayerGroup {
-	pub name: String,
-	pub children: Vec<Box<dyn SpriteLayer>>,
+	#[test]
+	fn it_adds() {
+		// assert_eq!(Vector2::new(1.0, 1.0) + Vector2::new(1.0, 1.0), Vector2::new(2.0, 2.0));
+		// assert_eq!(Vector2::new(0.0, 0.0) + 2.0, Vector2::new(2.0, 2.0));
+		// let mut v1 = Vector2::new(0.0, 0.0);
+		// v1 += 2.0;
+		// assert_eq!(v1, Vector2::new(2.0, 2.0));
+	}
 }
-
-impl SpriteLayer for SpriteLayerGroup {}
-
-pub struct SpriteLayerDataRGBA {
-	pub name: String,
-	pub width: i32,
-	pub height: i32,
-	pub data: Vec<i32>,
-}
-
-impl SpriteLayer for SpriteLayerDataRGBA {}
-
-pub struct SpriteLayerDataI {
-	pub name: String,
-	pub width: i32,
-	pub height: i32,
-	pub data: Vec<i8>,
-}
-
-impl SpriteLayer for SpriteLayerDataI {}
-
-pub struct SpriteLayerDataUV {
-	pub name: String,
-	pub width: i32,
-	pub height: i32,
-	pub data: Vec<i32>,
-}
-
-impl SpriteLayer for SpriteLayerDataUV {}
--- math/src/lib.rs
+++ math/src/lib.rs
@@ -1,6 +1,17 @@
-#![allow(dead_code)]
+pub use nalgebra_glm::*;
 
-pub mod vector2;
-pub mod vector3;
-pub mod matrix3;
-pub mod matrix4;
+#[derive(PartialEq, Clone)]
+pub struct Rect {
+	pub min: Vec2,
+	pub max: Vec2
+}
+
+impl Rect {
+	pub fn new(min: Vec2, max: Vec2) -> Rect {
+		Rect { min: min, max: max }
+	}
+
+	pub fn center(&self) -> Vec2 {
+		self.min + self.max / 2.
+	}
+}
