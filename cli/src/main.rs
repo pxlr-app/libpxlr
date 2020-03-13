82fe3e3a50a443953306bbe298d6ca236fc3fb32
Michael Grenier <grenim01@uqat.ca>
2020-03-13 13:51:46 -0400

    Gettin somewhere

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,7 +2,7 @@ use std::rc::Rc;
 use std::fmt::{Display, Formatter, Result};
 
 trait Patchable: Display {
-	fn patch(self, rnd: i32) -> Box<dyn Patchable>;
+	fn patch(&self, rnd: i32) -> Option<Self> where Self: Sized;
 }
 
 pub struct A {
@@ -16,25 +16,31 @@ impl Display for A {
 }
 
 impl Patchable for A {
-	fn patch(self, rnd: i32) -> Box<dyn Patchable> {
+	fn patch(&self, rnd: i32) -> Option<A> {
 		if rnd > 5 {
-			Box::new(A { name: Rc::clone(&self.name) })
+			Some(A { name: Rc::clone(&self.name) })
 		} else {
-			Box::new(self)
+			None
 		}
 	}
 }
 
 fn main() {
-	let str_A = Rc::new("AAA".to_owned());
-	println!("str_A count = {}", Rc::strong_count(&str_A));
-	let a = A { name: Rc::clone(&str_A) };
-	println!("a = {}, a.name count = {}", a, Rc::strong_count(&str_A));
+	let str_a = Rc::new("AAA".to_owned());
+	println!("str_a count = {}", Rc::strong_count(&str_a));
+	
+	let a0 = Rc::new(A { name: Rc::clone(&str_a) });
+	println!("str_a count = {}", Rc::strong_count(&str_a));
+
 	{
-		let a1 = a.patch(2);
-		println!("a1 = {}, a.name count = {}", a1, Rc::strong_count(&str_A));
+		let a1 = a0.patch(2).and_then(|p| Some(Rc::new(p))).or(Some(Rc::clone(&a0))).unwrap();
+		println!("str_a count = {}", Rc::strong_count(&str_a));
 	}
-	println!("a.name count = {}", Rc::strong_count(&str_A));
-	// let a2 = a.patch(8);
-	// println!("a2 = {}, a.name count = {}", a2, Rc::strong_count(&str_A));
+	
+	{
+		let a2 = a0.patch(8).unwrap();
+		println!("str_a count = {}", Rc::strong_count(&str_a));
+	}
+
+	println!("str_a count = {}", Rc::strong_count(&str_a));
 }
