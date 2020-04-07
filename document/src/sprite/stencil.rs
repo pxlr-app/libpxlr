aecb3e6751a118f812df6870307be9e4bba2d4b3
Michael Grenier <michael@grenier.dev>
2020-04-07 11:29:47 -0400

    Added Stencil

--- document/src/sprite/stencil.rs
+++ document/src/sprite/stencil.rs
@@ -1,10 +1,9 @@
 use math::Extent2;
-use std::rc::Rc;
 
 pub struct Stencil<T> {
-	pub size: Rc<Extent2<u16>>,
-	pub mask: Rc<Vec<u8>>,
-	pub data: Rc<Vec<T>>,
+	pub size: Extent2<u16>,
+	pub mask: Vec<u8>,
+	pub data: Vec<T>,
 }
 
 pub struct StencilDataIterator<'a, T> {
@@ -20,23 +19,20 @@ impl<'a, T> Iterator for StencilDataIterator<'a, T> {
 	type Item = (u32, u32, &'a T);
 
 	fn next(&mut self) -> Option<(u32, u32, &'a T)> {
-		let bit_offset = self.bit_offset;
-		let bit_count = (self.width * self.height * 8) as u32;
-		if bit_offset >= bit_count {
-			None
-		} else {
+		let bit_count = (self.mask.len() * 8) as u32;
+		while self.bit_offset < bit_count {
+			let bit_offset = self.bit_offset;
 			self.bit_offset += 1;
 			let uchar_offset = (bit_offset / 8) | 0;
-			let bit = !!(self.mask[uchar_offset as usize] & (1 << (bit_offset - uchar_offset * 8)));
-			if bit == 1 {
+			let bit = self.mask[uchar_offset as usize] & (1 << (bit_offset - uchar_offset * 8));
+			if bit != 0 {
 				let x = bit_offset % self.width as u32;
 				let y = (bit_offset / self.width as u32) | 0;
 				self.data_offset += 1;
-				Some((x, y, &self.data[(self.data_offset - 1) as usize]))
-			} else {
-				None
+				return Some((x, y, &self.data[(self.data_offset - 1) as usize]));
 			}
 		}
+		return None;
 	}
 }
 
@@ -44,19 +40,22 @@ impl<T> Stencil<T>
 where
 	T: Clone,
 {
-	pub fn new(size: Extent2<u16>, mask: Vec<u8>, data: Vec<T>) -> Stencil<T> {
-		Stencil::<T> {
-			size: Rc::new(size),
-			mask: Rc::new(mask),
-			data: Rc::new(data),
-		}
-	}
-
 	pub fn from_buffer(size: Extent2<u16>, buffer: &[T]) -> Stencil<T> {
-		let closest_bytes = (size.w * size.h) / 8;
-		let mask: Vec<u8> = vec![255; closest_bytes as usize];
+		assert_eq!((size.w * size.h) as usize, buffer.len());
+		let closest_bytes = 1 + (((size.w * size.h) - 1) / 8); // ceil
+		let mut mask: Vec<u8> = vec![255u8; closest_bytes as usize];
+		for i in buffer.len()..mask.len() * 8 {
+			let p = (i / 8) | 0;
+			let m = 1 << (i - p * 8);
+			mask[p] ^= m;
+		}
+
 		let data: Vec<T> = buffer.to_vec();
-		Stencil::<T>::new(size, mask, data)
+		Stencil::<T> {
+			size: size,
+			mask: mask,
+			data: data,
+		}
 	}
 
 	pub fn iter(&self) -> StencilDataIterator<T> {
@@ -68,5 +67,42 @@ where
 			mask: &self.mask[..],
 			data: &self.data[..],
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn it_from_buffer() {
+		let s = Stencil::from_buffer(Extent2::new(2, 2), &[1u8, 2u8, 3u8, 4u8]);
+		assert_eq!(*s.mask, [15u8]); // layout ⠛
+		assert_eq!(*s.data, [1u8, 2u8, 3u8, 4u8]);
+	}
+
+	#[test]
+	fn it_iter() {
+		let s = Stencil {
+			size: Extent2::new(2, 2),
+			mask: vec![15u8], // layout ⠛
+			data: vec![1u8, 2u8, 3u8, 4u8],
+		};
+		let mut i = s.iter();
+		assert_eq!(i.next(), Some((0, 0, &1u8)));
+		assert_eq!(i.next(), Some((1, 0, &2u8)));
+		assert_eq!(i.next(), Some((0, 1, &3u8)));
+		assert_eq!(i.next(), Some((1, 1, &4u8)));
+		assert_eq!(i.next(), None);
+
+		let s = Stencil {
+			size: Extent2::new(2, 2),
+			mask: vec![9u8], // layout ⠑
+			data: vec![1u8, 4u8],
+		};
+		let mut i = s.iter();
+		assert_eq!(i.next(), Some((0, 0, &1u8)));
+		assert_eq!(i.next(), Some((1, 1, &4u8)));
+		assert_eq!(i.next(), None);
 	}
 }
