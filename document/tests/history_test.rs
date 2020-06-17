4e74d7df012325e4f29f8bebf17e8cc36d842a35
Michael Grenier <michael@grenier.dev>
2020-06-16 20:17:24 -0400

    Added comment to history test

--- document/tests/history_test.rs
+++ document/tests/history_test.rs
@@ -8,9 +8,11 @@ use uuid::Uuid;
 
 #[test]
 fn it_moves_forward_and_backward() {
+	// Init note with A
 	let doc = Rc::new(Note::new(None, "A", Vec2::new(0., 0.)));
 	let mut his = History::new();
 
+	// Push rename to A->B
 	let (redo, undo) = doc.rename("B").unwrap();
 	his.add(Event {
 		hash: Uuid::new_v4(),
@@ -20,10 +22,12 @@ fn it_moves_forward_and_backward() {
 		undo: Box::new(undo),
 	});
 
+	// Move forward A->B
 	let event = his.travel_forward().unwrap();
 	let doc_a = doc.patch(&*event.redo).unwrap();
 	assert_eq!(*doc_a.note, "B");
 
+	// Push rename to B->C
 	let (redo, undo) = doc_a.rename("C").unwrap();
 	his.add(Event {
 		hash: Uuid::new_v4(),
@@ -33,32 +37,42 @@ fn it_moves_forward_and_backward() {
 		undo: Box::new(undo),
 	});
 
+	// Move forward B->C
 	let event = his.travel_forward().unwrap();
 	let doc_b = doc_a.patch(&*event.redo).unwrap();
 	assert_eq!(*doc_b.note, "C");
 
+	// At the end of time
 	assert!(his.travel_forward().is_none());
 
+	// Move backward C->B
 	let event = his.travel_backward().unwrap();
 	let doc_c = doc_b.patch(&*event.undo).unwrap();
 	assert_eq!(*doc_c.note, "B");
 
+	// Move backward B->A
 	let event = his.travel_backward().unwrap();
 	let doc_d = doc_c.patch(&*event.undo).unwrap();
 	assert_eq!(*doc_d.note, "A");
 
+	// At the beginning of time
 	assert!(his.travel_backward().is_none());
 
+	// Forget history
 	his.forget();
 
+	// No history
 	assert!(his.travel_forward().is_none());
+	assert!(his.travel_backward().is_none());
 }
 
 #[test]
 fn it_reorder_events() {
+	// Init note with A
 	let doc = Rc::new(Note::new(None, "A", Vec2::new(0., 0.)));
 	let mut his = History::new();
 
+	// Push rename A->B at T2
 	let (redo, undo) = doc.rename("B").unwrap();
 	his.add(Event {
 		hash: Uuid::new_v4(),
@@ -68,10 +82,12 @@ fn it_reorder_events() {
 		undo: Box::new(undo),
 	});
 
+	// Move forward A->B
 	let event = his.travel_forward().unwrap();
 	let doc_a = doc.patch(&*event.redo).unwrap();
 	assert_eq!(*doc_a.note, "B");
 
+	// Push rename B->C at T1
 	let (redo, undo) = doc_a.rename("C").unwrap();
 	his.add(Event {
 		hash: Uuid::new_v4(),
@@ -81,21 +97,27 @@ fn it_reorder_events() {
 		undo: Box::new(undo),
 	});
 
+	// Move forward B->C
 	let event = his.travel_forward().unwrap();
 	let doc_b = doc_a.patch(&*event.redo).unwrap();
 	assert_eq!(*doc_b.note, "C");
 
+	// At the end of time
 	assert!(his.travel_forward().is_none());
 
+	// Reorder history (consume old history)
 	let mut his = his.into_chronological();
 
+	// Move forward A->C
 	let event = his.travel_forward().unwrap();
 	let doc_a = doc.patch(&*event.redo).unwrap();
 	assert_eq!(*doc_a.note, "C");
 
+	// Move forward C->B
 	let event = his.travel_forward().unwrap();
 	let doc_b = doc_a.patch(&*event.redo).unwrap();
 	assert_eq!(*doc_b.note, "B");
 
+	// At the end of time
 	assert!(his.travel_forward().is_none());
 }
