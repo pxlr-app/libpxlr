ed373a4e4408abb5178882d03db5e229349eb373
Michael Grenier <michael@grenier.dev>
2020-11-01 15:39:24 -0500

    Moved to Snowpack

---
+++ app/snowpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	mount: {
+		public: '/',
+		src: '/_dist_',
+	},
+	plugins: [
+		'@snowpack/plugin-react-refresh',
+		'@snowpack/plugin-dotenv',
+		'@snowpack/plugin-typescript',
+		'@snowpack/plugin-webpack'
+	],
+	install: [
+		/* ... */
+	],
+	installOptions: {
+		/* ... */
+	},
+	devOptions: {
+		/* ... */
+	},
+	buildOptions: {
+		/* ... */
+	},
+	proxy: {
+		/* ... */
+	},
+	alias: {
+		/* ... */
+	},
+};
