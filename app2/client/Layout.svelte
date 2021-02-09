bdd52ad6592fbbceae71839d88e143866c94d87d
Michael Grenier <michael@grenier.dev>
2021-02-08 21:17:11 -0500

    Converted 95% of Layout component

--- app2/client/Layout.svelte
+++ app2/client/Layout.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
-	import type { Corner, Edge, PaneProps } from './helpers/Layout';
+	import type { Corner, PaneProps, Axe } from './helpers/Layout';
 	import { Layout } from './helpers/Layout';
+	import { nanoid } from 'nanoid';
 	import { onMount } from 'svelte';
-	import { each } from 'svelte/internal';
-
-	
 
 	export let panes: PaneProps[];
 
 	let layoutRef: HTMLDivElement | undefined;
 	let bounds = new DOMRect();
 	let dragging = false;
+	let dragLeft: string | undefined;
+	let dragRight: string | undefined;
+	let dragCorner: Corner | undefined;
 	let dragBounds = new DOMRect();
+	let dragSiblings = false;
+	let divId: number | undefined;
+	let divAxe: Axe | undefined;
+	let lastPointerEvent: PointerEvent | undefined;
+	let trottledPointerEvent: PointerEvent | undefined;
 	let layout = new Layout(panes);
 
 	console.log('Layout', layout);
@@ -26,13 +32,120 @@
 
 	function onLeave() {
 		if (dragging) {
+			dragging = false;
 
+			const newPanes = panes.reduce((panes, pane) => {
+				const layoutPane = layout.panes.find(p => p.id === pane.key);
+				if (layoutPane && layoutPane.width > 0 && layoutPane.height > 0) {
+					pane.top = layoutPane.top;
+					pane.right = layoutPane.right;
+					pane.bottom = layoutPane.bottom;
+					pane.left = layoutPane.left;
+					panes.push(pane);
+				}
+				return panes;
+			}, [] as PaneProps[]);
+
+			console.log('onChange', newPanes);
+			// TODO onChange && onChange(newPanes);
 		}
 	}
 
-	function onMove() {
+	function onMove(e: PointerEvent) {
 		if (dragging) {
+			if (e.clientX === lastPointerEvent?.clientX && e.clientY === lastPointerEvent?.clientY) {
+				return;
+			}
+			lastPointerEvent = e;
+			trottledPointerEvent = trottledPointerEvent ?? e;
 
+			const { x: oX, y: oY, width, height } = bounds;
+			const [x, y] = [e.clientX - oX, e.clientY - oY];
+			const [pX, pY] = [x / width * 100, y / height * 100];
+			const [cX, cY] = [
+				Math.max(Math.min(pX, dragBounds.right), dragBounds.left),
+				Math.max(Math.min(pY, dragBounds.bottom), dragBounds.top),
+			];
+
+			if (dragCorner !== undefined) {
+				const deltaX = e.clientX - trottledPointerEvent.clientX;
+				const deltaY = e.clientY - trottledPointerEvent.clientY;
+
+				if (deltaX * deltaX + deltaY * deltaY >= 20 * 20) {
+					trottledPointerEvent = e;
+
+					const axe = Math.abs(deltaX) > Math.abs(deltaY) ? 'vertical' : 'horizontal';
+
+					if (axe !== divAxe) {
+						divAxe = axe;
+
+						const newPanes = panes.reduce((panes, pane) => {
+							const newPane = { ...pane };
+							if (newPane.key === dragLeft || newPane.key === dragRight) {
+								newPane.top = dragBounds.top;
+								newPane.right = dragBounds.right;
+								newPane.bottom = dragBounds.bottom;
+								newPane.left = dragBounds.left;
+
+								if (axe === 'horizontal') {
+									newPane[newPane.key === dragLeft ? 'bottom' : 'top'] = cY;
+								} else {
+									newPane[newPane.key === dragLeft ? 'right' : 'left'] = cX;
+								}
+							}
+							panes.push(newPane);
+							return panes;
+						}, [] as PaneProps[]);
+
+						console.log('onChange', newPanes);
+						// TODO : onChange && onChange(newPanes);
+						return;
+					}
+				}
+			}
+
+			if (dragLeft !== undefined && dragRight !== undefined) {
+				const edge = layout.edges.find(e => e.left.id === dragLeft && e.right.id === dragRight);
+				if (edge) {
+					const p = edge.axe === 'horizontal' ? cY : cX;
+					
+					edge.p = p;
+					edge.updateDOM();
+
+					if (dragSiblings) {
+						for (const sibling of edge.allSiblings()) {
+							sibling.p = p;
+							sibling.updateDOM();
+						}
+					}
+
+					const newPanes = panes.map(pane => {
+						const newPane = { ...pane };
+						const layoutPane = layout.panes.find(p => p.id === pane.key);
+						if (layoutPane) {
+							newPane.top = layoutPane.top;
+							newPane.right = layoutPane.right;
+							newPane.bottom = layoutPane.bottom;
+							newPane.left = layoutPane.left;
+						}
+						return newPane
+					});
+
+					console.log('onDragging', newPanes);
+					// TODO
+					// onDragging && onDragging(panes.map(pane => {
+					// 	const newPane = { ...pane };
+					// 	const layoutPane = layout.panes.find(p => p.id === pane.key);
+					// 	if (layoutPane) {
+					// 		newPane.top = layoutPane.top;
+					// 		newPane.right = layoutPane.right;
+					// 		newPane.bottom = layoutPane.bottom;
+					// 		newPane.left = layoutPane.left;
+					// 	}
+					// 	return newPane
+					// }));
+				}
+			}
 		}
 	}
 
@@ -40,6 +153,28 @@
 		return (e: PointerEvent) => {
 			e.preventDefault();
 			e.stopPropagation();
+
+			const edge = layout.edges[id];
+			let minX: number = 0;
+			let maxX: number = 100;
+			let minY: number = 0;
+			let maxY: number = 100;
+			const breakable = edge.axe === 'horizontal'
+				? Math.abs(edge.left.left - edge.right.left) < 0.1 && Math.abs(edge.left.right - edge.right.right) < 0.1
+				: Math.abs(edge.left.top - edge.right.top) < 0.1 && Math.abs(edge.left.bottom - edge.right.bottom) < 0.1;
+
+			for (const sibiling of [edge].concat(edge.allSiblings())) {
+				minX = Math.max(minX, sibiling.left.left);
+				maxX = Math.min(maxX, sibiling.right.right);
+				minY = Math.max(minY, sibiling.left.top);
+				maxY = Math.min(maxY, sibiling.right.bottom);
+			}
+
+			dragging = true;
+			dragLeft = edge.left.id;
+			dragRight = edge.right.id;
+			dragBounds = new DOMRect(minX, minY, maxX - minX, maxY - minY);
+			dragSiblings = !(breakable && e.ctrlKey);
 		}
 	}
 
@@ -47,6 +182,34 @@
 		return (e: PointerEvent) => {
 			e.preventDefault();
 			e.stopPropagation();
+
+			const pane = layout.panes[id];
+
+			const paneProps: PaneProps = panes.find(p => p.key === pane.id)!;
+			const newPaneProps: PaneProps = { ...paneProps };
+			newPaneProps.key = nanoid();
+
+			dragLeft = pane.id;
+			dragRight = newPaneProps.key;
+
+			if (corner === 'top-left' || corner === 'bottom-left') {
+				let t = dragLeft;
+				dragLeft = dragRight;
+				dragRight = t;
+				paneProps.left += 1;
+				newPaneProps.right = paneProps.left;
+			} else {
+				paneProps.right -= 1;
+				newPaneProps.left = paneProps.right;
+			}
+
+			dragging = true,
+			dragCorner = corner;
+			dragBounds = new DOMRect(pane.left, pane.top, pane.right - pane.left, pane.bottom - pane.top);
+			divAxe = 'vertical';
+
+			console.log('onChange', panes.concat([newPaneProps]));
+			// TODO onChange && onChange(panes.concat([newPaneProps]));
 		}
 	}
 </script>
