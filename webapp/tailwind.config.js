0840775e7f458e0f0738ada44602c9ebfc6afc3a
Michael Grenier <michael@grenier.dev>
2021-05-08 14:51:49 -0400

    Scaffolded webapp

---
+++ webapp/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+	purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
