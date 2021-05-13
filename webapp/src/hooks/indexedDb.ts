287db874dc3d6c9ddbef91d2745b469046662247
Michael Grenier <michael@grenier.dev>
2021-05-12 21:39:22 -0400

    Refactor local file DB

--- webapp/src/hooks/indexedDb.ts
+++ webapp/src/hooks/indexedDb.ts
@@ -6,7 +6,7 @@ const cacheIndexedDb = new Map<string, () => IDBDatabase>();
  * Retrieve an IDBDatabase
  */
 export function useDatabase(name: string, options?: { version?: number, onUpgradeNeeded?: (this: IDBOpenDBRequest, ev: IDBVersionChangeEvent) => any }): IDBDatabase {
-	let key = `${name}:${options?.version ?? 0}`;
+	const key = `${name}:${options?.version ?? 0}`;
 	if (!cacheIndexedDb.has(key)) {
 		cacheIndexedDb.set(key, suspend(new Promise<IDBDatabase>((resolve, reject) => {
 			let request = indexedDB.open(name, options?.version ?? 0);
@@ -19,7 +19,7 @@ export function useDatabase(name: string, options?: { version?: number, onUpgrad
 			};
 		})))
 	}
-	let suspender = cacheIndexedDb.get(key)!;
+	const suspender = cacheIndexedDb.get(key)!;
 	// Suspend till first onAuthStateChanged
 	return suspender();
 }
--- webapp/src/pages/FileApi.tsx
+++ webapp/src/pages/FileApi.tsx
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from "react";
 import { useDatabase } from "../hooks/indexedDb";
 
 export default function FileAPI() {
-	let db = useDatabase("test", {
+	const db = useDatabase("test", {
 		version: 1,
 		onUpgradeNeeded: function () {
-			this.result.createObjectStore("localfiles");
+			const store = this.result.createObjectStore("handles");
+			store.createIndex("handles_mtime", "mtime");
 		},
 	});
+	const [dirty, setDirty] = useState(0);
 	// https://web.dev/file-system-access/#storing-file-handles-or-directory-handles-in-indexeddb
-	let [files, setFiles] = useState<any[]>([]);
+	const [items, setItems] = useState<any[]>([]);
 	useEffect(() => {
-		let tx = db.transaction("localfiles", "readonly");
-		let store = tx.objectStore("localfiles");
-		let request = store.getAll();
+		const tx = db.transaction("handles", "readonly");
+		const store = tx.objectStore("handles");
+		const request = store.getAll();
 		request.onsuccess = (e) => {
-			setFiles(request.result);
+			setItems(request.result);
 		};
-	}, [db]);
+	}, [db, dirty]);
 	useEffect(() => {
 		function onDragOver(e: DragEvent) {
 			e.preventDefault();
@@ -25,16 +27,22 @@ export default function FileAPI() {
 		async function onDrop(e: DragEvent) {
 			e.stopPropagation();
 			e.preventDefault();
-			for (let item of e.dataTransfer?.items ?? []) {
-				let handle = await (item as any).getAsFileSystemHandle();
-				let tx = db.transaction("localfiles", "readwrite");
-				let store = tx.objectStore("localfiles");
+			for (const item of e.dataTransfer?.items ?? []) {
+				const handle = await (item as any).getAsFileSystemHandle();
+				const tx = db.transaction("handles", "readwrite");
+				const store = tx.objectStore("handles");
 				await new Promise<void>((resolve, reject) => {
-					let request = store.put(handle, handle.name);
+					const data = {
+						name: handle.name,
+						handle,
+						mtime: new Date().getTime(),
+					};
+					const request = store.put(data, handle.name);
 					request.onsuccess = (e) => resolve();
 					request.onerror = (e) => reject();
 				});
 			}
+			setDirty((dirty) => dirty + 1);
 		}
 
 		document.addEventListener("dragover", onDragOver);
@@ -47,31 +55,31 @@ export default function FileAPI() {
 	});
 	return (
 		<ul>
-			{files.map((item) => {
-				console.debug(item);
+			{items.map((item) => {
 				return (
 					<li key={item.name}>
 						<a
 							onClick={async (e) => {
 								e.preventDefault();
-								await item.requestPermission({
-									mode: "readwrite",
+								await item.handle.requestPermission({
+									mode: "read",
 								});
-								let file: File = await item.getFile();
-								let blob = file.slice(0, 10);
-								let reader = new FileReader();
-								let buffer = await new Promise<ArrayBuffer>(
-									(resolve, reject) => {
-										reader.onload = (e) =>
-											resolve(
-												e.target!.result as ArrayBuffer,
-											);
-										reader.onerror = (e) => reject();
-										reader.readAsArrayBuffer(blob);
-									},
-								);
-								let decoder = new TextDecoder();
-								let text = decoder.decode(buffer);
+								const file: File = await item.handle.getFile();
+								// const blob = file.slice(0, 10);
+								// const buffer = await new Promise<ArrayBuffer>(
+								// 	(resolve, reject) => {
+								// 		const reader = new FileReader();
+								// 		reader.onload = (e) =>
+								// 			resolve(
+								// 				e.target!.result as ArrayBuffer,
+								// 			);
+								// 		reader.onerror = (e) => reject();
+								// 		reader.readAsArrayBuffer(blob);
+								// 	},
+								// );
+								const buffer = await file.arrayBuffer();
+								const decoder = new TextDecoder();
+								const text = decoder.decode(buffer);
 								console.log("Content:", text);
 							}}
 						>
