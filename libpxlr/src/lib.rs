4366ed91c1501bbe672045b520648e8dff23f9d2
Michael Grenier <michael@grenier.dev>
2021-05-17 13:07:01 -0400

    Impl Seek for FileReader

--- libpxlr/src/lib.rs
+++ libpxlr/src/lib.rs
@@ -1,8 +1,9 @@
+use async_std::io::prelude::SeekExt;
 use std::pin::Pin;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{
-	console, Blob, File, FileSystemFileHandle, FileSystemHandlePermissionDescriptor,
+	console, File, FileSystemFileHandle, FileSystemHandlePermissionDescriptor,
 	FileSystemPermissionMode, TextDecoder,
 };
 // use downcast::downcast_jsvalue;
@@ -22,9 +23,7 @@ pub fn main_js() -> Result<(), JsValue> {
 	console_error_panic_hook::set_once();
 
 	// Your code goes here!
-	unsafe {
-		console::log_1(&JsValue::from_str("Hello world!"));
-	}
+	console::log_1(&JsValue::from_str("Hello world!"));
 
 	Ok(())
 }
@@ -51,44 +50,35 @@ pub fn pxlr_hello_world(mut word: String) -> String {
 #[cfg(feature = "wasm")]
 #[wasm_bindgen]
 pub async fn pxlr_print_file(handle: FileSystemFileHandle) -> Result<(), JsValue> {
+	use async_std::io::ReadExt;
+	// Request permission to user
 	let state = JsFuture::from(handle.request_permission_with_descriptor(
 		FileSystemHandlePermissionDescriptor::new().mode(FileSystemPermissionMode::Read),
 	))
 	.await?;
-	unsafe {
-		console::log_2(&JsValue::from_str("Permission:"), &state);
-	}
+	// If permission granted
+	console::log_2(&JsValue::from_str("Permission:"), &state);
 	if state == JsValue::from_str("granted") {
+		// Retrieve File from handle
 		let file: File = JsFuture::from(handle.get_file()).await?.into();
-		use async_std::io::ReadExt;
+		// Create FileReader
 		let mut reader = FileReader::new(file);
-		let mut buffer: Vec<u8> = vec![0u8; 10];
+		// Retrieve first 10 bytes
+		let mut buffer: Vec<u8> = vec![0u8; 5];
+		let _ = reader.seek(async_std::io::SeekFrom::End(-5)).await;
 		let _ = reader.read_exact(&mut buffer).await;
+		// Decode bytes to UTF8 string
 		let decoder = TextDecoder::new().unwrap();
 		if let Ok(content) = decoder.decode_with_u8_array(&mut buffer[..]) {
-			unsafe {
-				console::log_2(&JsValue::from_str("Content:"), &JsValue::from_str(&content));
-			}
+			console::log_2(&JsValue::from_str("Content:"), &JsValue::from_str(&content));
 		}
-		// // Option 2
-		// let buffer = file.slice_with_i32_and_i32(0, 10).unwrap();
-		// let text = JsFuture::from(buffer.text()).await?;
-		// unsafe { console::log_2(&JsValue::from_str("Content:"), &text); }
-		// // Option 1
-		// let decoder = TextDecoder::new().unwrap();
-		// let buffer = JsFuture::from(file.array_buffer()).await?;
-		// if let Ok(content) = decoder.decode_with_buffer_source(&buffer.into()) {
-		// 	unsafe {
-		// 		console::log_2(&JsValue::from_str("Content:"), &JsValue::from_str(&content));
-		// 	}
-		// }
 	}
 	Ok(())
 }
 
 pub struct FileReader {
 	file: File,
-	offset: u64,
+	offset: i64,
 	future: Option<JsFuture>,
 }
 
@@ -99,6 +89,24 @@ impl FileReader {
 			offset: 0,
 			future: None,
 		}
+	}
+}
+
+impl async_std::io::Seek for FileReader {
+	fn poll_seek(
+		self: std::pin::Pin<&mut Self>,
+		_cx: &mut async_std::task::Context<'_>,
+		pos: async_std::io::SeekFrom,
+	) -> async_std::task::Poll<async_std::io::Result<u64>> {
+		let self_mut = self.get_mut();
+		let new_offset = match pos {
+			async_std::io::SeekFrom::Current(pos) => self_mut.offset + pos,
+			async_std::io::SeekFrom::End(pos) => pos as i64,
+			async_std::io::SeekFrom::Start(pos) => pos as i64,
+		};
+		// console::log_2(&JsValue::from_str("Offset:"), &JsValue::from_f64(new_offset as f64));
+		self_mut.offset = new_offset;
+		async_std::task::Poll::Ready(Ok(new_offset as u64))
 	}
 }
 
@@ -113,10 +121,17 @@ impl async_std::io::Read for FileReader {
 		let future = mut_self.future.take();
 		match future {
 			None => {
-				let blob: web_sys::Blob = mut_self
-					.file
-					.slice_with_i32_and_i32(mut_self.offset as i32, len as i32)
-					.unwrap();
+				// console::log_3(&JsValue::from_str("Slice:"), &JsValue::from_f64(mut_self.offset as f64), &JsValue::from_f64((mut_self.offset + len as i64) as f64));
+				let start: i64 = mut_self.offset;
+				let end: i64 = mut_self.offset + len as i64;
+				let blob: web_sys::Blob = if end != 0 {
+					mut_self
+						.file
+						.slice_with_i32_and_i32(start as i32, end as i32)
+						.unwrap()
+				} else {
+					mut_self.file.slice_with_i32(start as i32).unwrap()
+				};
 				let mut buffer = JsFuture::from(blob.array_buffer());
 				let pinned = Pin::new(&mut buffer);
 				// Poll future to initiate waker
@@ -133,6 +148,7 @@ impl async_std::io::Read for FileReader {
 					}
 					async_std::task::Poll::Ready(res) => {
 						let buffer = js_sys::Uint8Array::new(&res.unwrap());
+						// console::log_3(&JsValue::from_str("Bytes:"), &JsValue::from_f64(buffer.byte_length() as f64), &JsValue::from_f64(len as f64));
 						buffer.copy_to(buf);
 						async_std::task::Poll::Ready(Ok(len))
 					}
