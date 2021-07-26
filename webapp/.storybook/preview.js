d75c28c3d6b2c5fe54fab011e0b0f16555c5f9a2
Michael Grenier <michael@grenier.dev>
2021-07-25 21:15:31 -0400

    Fix Solidjs in Storybook

--- webapp/.storybook/preview.js
+++ webapp/.storybook/preview.js
@@ -13,14 +13,19 @@ export const parameters = {
 	},
 };
 
+let disposeStory;
+
 export const decorators = [
 	Story => {
+		if (disposeStory) {
+			disposeStory();
+		}
 		const root = document.getElementById("root");
 		const solid = document.createElement("div");
 		solid.setAttribute('id', 'solid-root');
 
 		root.appendChild(solid);
-		render(Story, solid);
+		disposeStory = render(Story, solid);
 		return solid;
 		// return createRoot(() => Story());
 	}
